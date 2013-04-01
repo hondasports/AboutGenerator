80ba55a1449b58d08441808b8a0f92ff971283cd
Tatsuya Miyamoto <hondasports@gmail.com>
2013-04-01 12:10:21 +0900

    Initial Commit

---
+++ AboutGenerator/AboutGenerator.h
@@ -0,0 +1,13 @@
+//
+//  AboutGenerator.h
+//  AboutGenerator
+//
+//  Created by MIYAMOTO TATSUYA on 2013/04/01.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AboutGenerator : NSObject
+
+@end
---
+++ AboutGenerator/AboutGenerator.m
@@ -0,0 +1,13 @@
+//
+//  AboutGenerator.m
+//  AboutGenerator
+//
+//  Created by MIYAMOTO TATSUYA on 2013/04/01.
+//
+//
+
+#import "AboutGenerator.h"
+
+@implementation AboutGenerator
+
+@end
